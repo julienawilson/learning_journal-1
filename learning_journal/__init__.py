18710677b5a0efbd7afcb667686529f77530af25
Maelle Vance <maellevance@gmail.com>
2016-12-28 16:25:17 -0800

    fixing heroku

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -5,8 +5,8 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application."""
-    config = Configurator(settings=settings)
     settings["sqlalchemy.url"] = os.environ["DATABASE_URL"]
+    config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
     config.include('.models')
     config.include('.routes')
