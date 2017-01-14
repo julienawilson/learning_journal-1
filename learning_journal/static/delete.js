e1582a0a890ca80c0d880bd048807152fa8c6a60
Maelle Vance <maellevance@gmail.com>
2017-01-14 12:02:23 -0800

    js script for delete

---
+++ learning_journal/static/delete.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    var deleters = $(".delete");
+    deleters.on("click", function(){
+        // send ajax request to delete this expense
+        $.ajax({
+            url: 'delete/' + $(this).attr("data") + '/delete',
+            success: function(){
+                console.log("deleted");
+            }
+        });        
+        // fade out expense
+        this_row = $(this.parentNode.parentNode);
+        // delete the containing row
+        this_row.animate({
+            opacity: 0
+        }, 500, function(){
+            $(this).remove();
+        })
+    });
+});
